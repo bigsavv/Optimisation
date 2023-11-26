75e91d7faf52fa1b34232b4a4c03935833811896
bigsavv <113073027+bigsavv@users.noreply.github.com>
2023-11-25 22:33:04 -0500

    html

--- viz.py
+++ viz.py
@@ -1,19 +1,16 @@
 import pandas as pd
-import plotly.express as px
-
 
 # Load the dataset
 df = pd.read_csv('resultats_A.csv')  # Replace with your file path
 
-
-
-
 # Aggregate data
 grouped_df = df.groupby(['Jour', 'Essence']).agg({'A': 'sum'}).reset_index()
 
-# Function to create a line plot based on a specific 'Foret'
+import plotly.express as px
+
+# Create a function to update the figure based on the selected 'Foret'
 def plot_foret(foret):
-    filtered_df = grouped_df[grouped_df['Foret'] == foret] if foret else grouped_df
+    filtered_df = df[df['Foret'] == foret] if foret else df
     fig = px.line(filtered_df, x='Jour', y='A', color='Essence',
                   labels={'Jour': 'Day', 'A': 'Value of A'},
                   title='Distribution and Trends of A over Time by Essence')
@@ -23,7 +20,7 @@ def plot_foret(foret):
 fig = plot_foret(None)
 
 # Add dropdown for filtering by 'Foret'
-foret_options = [{'label': foret, 'method': 'restyle', 'args': [{'visible': [foret == f for f in grouped_df['Foret']]}]} for foret in grouped_df['Foret'].unique()]
+foret_options = [{'label': foret, 'method': 'restyle', 'args': [{'visible': [foret == f for f in df['Foret']]}]} for foret in df['Foret'].unique()]
 fig.update_layout(updatemenus=[{
     'buttons': foret_options,
     'direction': 'down',
@@ -32,4 +29,3 @@ fig.update_layout(updatemenus=[{
 
 # Show the figure
 fig.show()
-
